9e91e9d34bd9bf160849ecdc9cb47ff8957e53a1
Juan Lagunas <dev.jlagunas@gmail.com>
2024-09-11 15:10:13 +0900

    feat(TodoDate): add current date functionality

--- src/components/TodoDate.vue
+++ src/components/TodoDate.vue
@@ -1,8 +1,23 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from "vue";
+
+// REF
+const currentDate = ref("");
+
+const formatDate = () => {
+  const date = new Date();
+  const options = { year: "numeric", month: "long", day: "numeric" };
+  currentDate.value = date.toLocaleDateString("en-US", options);
+};
+
+onMounted(() => {
+  formatDate();
+});
+</script>
 <template>
   <div
     class="bg-slate-800 text-slate-50 text-center py-1 absolute -top-3 left-4 w-max px-4 rounded-sm text-sm dark:text-slate-800 dark:bg-slate-50"
   >
-    <p>September 11 2024</p>
+    <p>{{ currentDate }}</p>
   </div>
 </template>
